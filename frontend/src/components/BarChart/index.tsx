149c3ac7a7f2dda243bfd0973369e0decbebc05c
Richard Brayan <richardbrayan2009@gmail.com>
2021-09-06 12:27:22 -0300

    Static charts

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -9,7 +9,7 @@ const BarChart = () => {
             }
         },
     };
-    
+
     const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -17,14 +17,14 @@ const BarChart = () => {
         series: [
             {
                 name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]
             }
         ]
     };
 
     return (
-        <Chart 
-            options={{... options, xaxis:mockData.labels,}}
+        <Chart
+            options={{ ...options, xaxis: mockData.labels}}
             series={mockData.series}
             type="bar"
             height="240"
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -6,7 +6,7 @@ const DonutChart = () => {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+
     const options = {
         legend: {
             show: true
@@ -14,8 +14,8 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{... options, labels:mockData.labels,}}
+        <Chart
+            options={{ ...options, labels: mockData.labels}}
             series={mockData.series}
             type="donut"
             height="240"
