755f6ee440dd04c9a05ff14d239d98252daa7f3a
Richard Brayan <richardbrayan2009@gmail.com>
2021-09-11 09:45:31 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,22 @@
+import Home from 'pages/Home';
+import DashBoard from 'pages/Home/Dashboard/indes';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+
+                <Route path="/dashboard">
+                    <DashBoard />
+                </Route>
+
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
